d6b756512d73b683301f6dc26e9bccb9435a51fb
Ítalo Martins <italomartinsg@gmail.com>
2024-08-15 15:30:13 -0300

    alterado caminho da fetch animais

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,4 +1,4 @@
-import AnimaNumeros from './anima-numeros.js';
+import AnimaNumeros from '../../animaisapi.json';
 
 export default function fetchAnimais(url, target) {
   // Cria a div contendo informações com o total de animais
